8853f1f24bb4afb6353b53bda5166db5d784ef5c
MuratKaya <97357050+muratkayaa@users.noreply.github.com>
2022-08-25 20:19:44 +0300

    Add files via upload

---
+++ Hata_Yonetimi/Hata_Yonetimi/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hata_Yonetimi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Bir Sayi Giriniz:");
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Sayiniz :" + sayi);
+
+            }
+            catch (Exception ex)
+            {
+
+               Console.WriteLine("Hataniz:" + ex.Message.ToString());
+            }
+            //finally 
+            //{ 
+            //    Console.WriteLine("islem taalanidi");
+            //}
+            try
+            {
+                //int a = int.Parse(null);
+                //int a =int.Parse("test");
+                int a = int.Parse("-20000000000000000");
+            }
+            catch (ArgumentNullException ex)
+            {
+
+                Console.WriteLine("Bos Deger Girdiniz");
+                Console.WriteLine(ex);
+            }
+            catch (FormatException ex)
+            {
+
+                Console.WriteLine("Format Uygun Degil");
+                Console.WriteLine(ex);
+            }
+            catch (OverflowException ex)
+            {
+
+                Console.WriteLine("Cok Kucuk veya Cok Buyuk Bir Deger Girdiniz.");
+                Console.WriteLine(ex);
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
